132310c635e4953b6bf4eef2ce229074e025a992
bongbak <2024112172@dgu.ac.kr>
2025-08-29 20:46:36 +0900

    회원가입, 로그인, 로그아웃 기능 수정

--- src/main/java/prestudy/team4/board/config/SecurityConfig.java
+++ src/main/java/prestudy/team4/board/config/SecurityConfig.java
@@ -4,31 +4,54 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
         http
-            .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/v1/**").permitAll()
-                .anyRequest().permitAll()
-            )
-            .formLogin(form -> form.disable())
-            .httpBasic(basic -> basic.disable());
+                .authorizeHttpRequests((auth)-> auth
+                        .requestMatchers("/api/v1/auth/login","/api/v1/auth/join").permitAll()
+                        .requestMatchers("/").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/user/**").hasAnyRole("ADMIN","USER")
+                        .requestMatchers("/my/**").hasAnyRole("ADMIN","USER")
+                        .anyRequest().authenticated()
+                );
+
+        http
+                .formLogin((auth)-> auth.loginPage("/api/v1/auth/login")
+                        .loginProcessingUrl("/api/v1/auth/loginProc")
+                        .permitAll()
+                );
+
+        http
+                .csrf((auth)->auth.disable());
+
+        http
+                .sessionManagement(
+                        (auth) -> auth
+                                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                                .sessionFixation((sessionFixation)->sessionFixation.newSession())
+                                .maximumSessions(1)
+                                .maxSessionsPreventsLogin(true)
+                );
+
+        http
+                .logout((auth)->auth.logoutUrl("/api/v1/auth/logout")
+                        .logoutSuccessUrl("/"));
 
         return http.build();
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
