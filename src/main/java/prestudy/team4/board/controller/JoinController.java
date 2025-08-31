ecf4ce7452be634ae16b84e5065285ba17e731f5
bongbak <2024112172@dgu.ac.kr>
2025-08-31 23:59:53 +0900

    Controller->RestController로 수정

--- src/main/java/prestudy/team4/board/controller/JoinController.java
+++ src/main/java/prestudy/team4/board/controller/JoinController.java
@@ -1,7 +1,6 @@
 package prestudy.team4.board.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
